8961b04132753ab01283c2d81a6607d5ee7881ad
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-09 21:59:49 +0100

    Modifie WebApplication and .gitignore

--- spring-web/src/main/java/it/edoardo/springweb/app/WebApplication.java
+++ spring-web/src/main/java/it/edoardo/springweb/app/WebApplication.java
@@ -1,9 +1,10 @@
 package it.edoardo.springweb.app;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc @Configuration
+@EnableWebMvc @Configuration @ComponentScan("it.edoardo.springweb.controller")
 public class WebApplication {
 
 }
