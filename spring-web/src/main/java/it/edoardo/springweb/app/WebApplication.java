f3371a57701e8b853ae3b8cc5d0baa134e6c3738
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-10 17:54:55 +0100

    Add Bean Database into the WebApplication container

--- spring-web/src/main/java/it/edoardo/springweb/app/WebApplication.java
+++ spring-web/src/main/java/it/edoardo/springweb/app/WebApplication.java
@@ -1,10 +1,21 @@
 package it.edoardo.springweb.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import it.edoardo.springweb.database.Database;
 
 @EnableWebMvc @Configuration @ComponentScan("it.edoardo.springweb.controller")
 public class WebApplication {
-
+	
+	@Bean(name = "database") @Scope("singleton")
+	@Description("Bean used to simulate a database")
+	public Database getDatabase() {
+		return new Database();
+	}
+	
 }
