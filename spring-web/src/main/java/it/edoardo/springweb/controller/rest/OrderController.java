4c0bcac3573e06e0e50370ced314aca9a80b3ebb
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-10 15:10:30 +0100

    Created new controllers

---
+++ spring-web/src/main/java/it/edoardo/springweb/controller/rest/OrderController.java
@@ -0,0 +1,8 @@
+package it.edoardo.springweb.controller.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "orders/")
+public class OrderController {
+
+}
---
+++ spring-web/src/main/java/it/edoardo/springweb/controller/rest/ProductController.java
@@ -0,0 +1,8 @@
+package it.edoardo.springweb.controller.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "products/")
+public class ProductController {
+
+}
