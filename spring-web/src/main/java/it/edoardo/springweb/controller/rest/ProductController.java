d2ec7c19b70f37c46ce9f98973b4d93526a3008d
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-11 16:22:54 +0100

    Update ProductController with REST methods

--- spring-web/src/main/java/it/edoardo/springweb/controller/rest/ProductController.java
+++ spring-web/src/main/java/it/edoardo/springweb/controller/rest/ProductController.java
@@ -17,6 +17,11 @@ public class ProductController {
 	
 	@Autowired private Database database;
 	
+	/**
+	 * URL => http://localhost:8080/spring-web/products/
+	 * METHOD => GET
+	 * @return all the element inside the collection 
+	 */
 	@GetMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String getProducts() {
 		StringBuilder strBuilder = new StringBuilder();
@@ -28,6 +33,11 @@ public class ProductController {
 		return strBuilder.toString();
 	}
 	
+	/**
+	 * URL => http://localhost:8080/spring-web/products/:id
+	 * METHOD => GET
+	 * @return the element with the corresponding id
+	 */
 	@GetMapping(path = "/{productId}", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String getProduct(@PathVariable("productId") String productId) {
 		final int productIndex = Integer.parseInt(productId);
@@ -37,16 +47,56 @@ public class ProductController {
 		return "There is not any product with that id!";
 	}
 	
+	/**
+	 * Add a new product in the collectiom
+	 * URL => http://localhost:8080/spring-web/products/
+	 * METHOD => POST
+	 * @return the new collection
+	 */
 	@PostMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String addProduct() {
 		return null;
 	}
 	
+	/**
+	 * Replace the collection with a new one
+	 * URL => http://localhost:8080/spring-web/products/
+	 * METHOD => PUT
+	 * @return the new collection
+	 */
 	@PutMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
-	public String updateProduct() {
+	public String updateProducts() {
 		return null;
 	}
 	
+	/**
+	 * Replace the element in the collection, or creates a new one
+	 * URL => http://localhost:8080/spring-web/products/:id
+	 * METHOD => PUT
+	 * @return the new collection
+	 */
+	@PutMapping(path = "/{productId}", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String updateProduct(@PathVariable("productId") String productId) {
+		return null;
+	}
+	
+	/**
+	 * Delete all the collection
+	 * URL => http://localhost:8080/spring-web/products/:id
+	 * METHOD => DELETE
+	 * @return the empty collection
+	 */
+	@DeleteMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String deleteProducts() {
+		return null;
+	}
+	
+	/**
+	 * Delete a product in the collection
+	 * URL => http://localhost:8080/spring-web/products/:id
+	 * METHOD => DELETE
+	 * @return the new collection
+	 */
 	@DeleteMapping(path = "/{productId}", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String deleteProduct(@PathVariable("productId") String productId) {
 		return productId;
