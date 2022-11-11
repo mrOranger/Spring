b1c00ca085151e3c5ba5cc39834061b01de01d0d
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-11 16:17:49 +0100

    Add JSON dependency and setup routes for REST in OrderController

--- spring-web/src/main/java/it/edoardo/springweb/controller/rest/OrderController.java
+++ spring-web/src/main/java/it/edoardo/springweb/controller/rest/OrderController.java
@@ -17,6 +17,11 @@ public class OrderController {
 	
 	@Autowired private Database database;
 	
+	/**
+	 * URL => http://localhost:8080/spring-web/orders/
+	 * METHOD => GET 
+	 * @return the list of the orders of the collection
+	 */
 	@GetMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String getOrders() {
 		StringBuilder strBuilder = new StringBuilder();
@@ -28,6 +33,11 @@ public class OrderController {
 		return strBuilder.toString();
 	}
 	
+	/**
+	 * URL => http://localhost:8080/spring-web/orders/:id
+	 * METHOD => GET 
+	 * @return the order with the corresponding id if present in the collection
+	 */
 	@GetMapping(path = "/{orderId}", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String getOrder(@PathVariable("orderId") String orderId) {
 		final int orderIndex = Integer.parseInt(orderId);
@@ -37,16 +47,59 @@ public class OrderController {
 		return "There is not any order with that id!";
 	}
 	
+	/**
+	 * Creates a new element in the collection
+	 * URL => http://localhost:8080/spring-web/orders/
+	 * METHOD => POST 
+	 * @return the new collection
+	 */
 	@PostMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String addOrder() {
 		return null;
 	}
 	
+	/**
+	 * Substitutes the collection with a new one
+	 * URL => http://localhost:8080/spring-web/orders/
+	 * METHOD => PUT 
+	 * @return the new collection
+	 */
 	@PutMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String updateOrders() {
+		return null;
+	}
+	
+	/**
+	 * Substitutes the order with the corresponding id if is in the collection,
+	 * otherwise creates a new one.
+	 * URL => http://localhost:8080/spring-web/orders/:id
+	 * METHOD => PUT 
+	 * @return the modified collection
+	 */
+	@PutMapping(path = "/{orderId}", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String updateOrder() {
 		return null;
 	}
 	
+	/**
+	 * Deletes the element in the collection
+	 * otherwise creates a new one.
+	 * URL => http://localhost:8080/spring-web/orders/
+	 * METHOD => DELETE
+	 * @return an empty collection
+	 */
+	@DeleteMapping(path = "/", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String deleteOrders() {
+		return null;
+	}
+	
+	/**
+	 * Deletes the element in the collection
+	 * otherwise creates a new one.
+	 * URL => http://localhost:8080/spring-web/orders/:id
+	 * METHOD => DELETE
+	 * @return the modified collection
+	 */
 	@DeleteMapping(path = "/{orderId}", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String deleteOrder(@PathVariable("orderId") String orderId) {
 		return orderId;
