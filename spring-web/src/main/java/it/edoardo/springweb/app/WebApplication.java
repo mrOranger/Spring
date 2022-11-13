b5d62c556961a09b2a2690abe42e0aa1bc3f6c4d
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-13 12:50:50 +0100

    Insert new Bean, and declared autowired attributes in Database

--- spring-web/src/main/java/it/edoardo/springweb/app/WebApplication.java
+++ spring-web/src/main/java/it/edoardo/springweb/app/WebApplication.java
@@ -1,5 +1,9 @@
 package it.edoardo.springweb.app;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +11,13 @@ import org.springframework.context.annotation.Description;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.google.common.collect.Lists;
+
 import it.edoardo.springweb.database.Database;
+import it.edoardo.springweb.model.Item;
+import it.edoardo.springweb.model.Order;
+import it.edoardo.springweb.model.Product;
+import it.edoardo.springweb.model.User;
 
 @EnableWebMvc @Configuration @ComponentScan("it.edoardo.springweb.controller")
 public class WebApplication {
@@ -18,4 +28,50 @@ public class WebApplication {
 		return new Database();
 	}
 	
+	@Bean(name = "user") @Scope("prototype")
+	@Description("Simple Bean of an user item")
+	public Item getUser(String firstName, String lastName) {
+		return new User(firstName, lastName);
+	}
+	
+	@Bean(name = "product") @Scope("prototype")
+	@Description("Simple bean of a product item")
+	public Item getProduct(String productName) {
+		return new Product(productName);
+	}
+	
+	@Bean(name = "order") @Scope("prototype")
+	@Description("Simple bean of an order item")
+	public Item getOrder(List<Item> products, Item user) {
+		return new Order(products, user);
+	}
+	
+	@Bean(name = "users") @Scope("singleton")
+	@Description("Bean containing the list of users in this application to inject in Database Bean")
+	public List<Item> getUsers() {
+		return Lists.newArrayList(getUser("Mario", "Rossi"), 
+				getUser("Maria", "Neri"),
+				getUser("Federico", "Verdi"),
+				getUser("Federica", "Bruni"));
+	}
+	
+	@Bean(name = "products") @Scope("singleton")
+	@Description("Bean containing the list of products in this application to inject in Database Bean")
+	public List<Item> getProducts() {
+		return Lists.newArrayList(new Product("Ammoniaca"),
+				new Product("Pane"),
+				new Product("Candeggina"),
+				new Product("Acqua"),
+				new Product("Zucchine"),
+				new Product("Pasta"));
+	}
+	
+	@Bean(name = "products") @Scope("singleton")
+	@Description("Bean containing the list of orders in this application to inject in Database Bean")
+	public List<Item> getOrders() {
+		return Lists.newArrayList(new Order(getProducts().subList(2, 5), getUsers().get(0)),
+				new Order(getProducts().subList(1, 3), getUsers().get(1)),
+				new Order(getProducts().subList(3, 5), getUsers().get(2)),
+				new Order(getProducts().subList(0, 5), getUsers().get(3)));
+	}
 }
--- spring-web/src/main/java/it/edoardo/springweb/database/Database.java
+++ spring-web/src/main/java/it/edoardo/springweb/database/Database.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import it.edoardo.springweb.model.Item;
@@ -16,42 +17,39 @@ import it.edoardo.springweb.model.User;
 @Component
 public class Database {
 	
-	private List<Item> users;
-	private List<Item> products;
-	private List<Item> orders;
+	@Autowired private List<Item> users;
+	@Autowired private List<Item> products;
+	@Autowired private List<Item> orders;
 	
 	@PostConstruct
 	public void init() {
-		this.createUsers();
-		this.creteProducts();
-		this.createOrders();
 	}
 	
-	private void createUsers() {
-		this.users = new ArrayList<Item>();
-		this.users.add(new User("Mario", "Rossi"));
-		this.users.add(new User("Maria", "Rossi"));
-		this.users.add(new User("Federico", "Rossi"));
-		this.users.add(new User("Federica", "Rossi"));
-	}
-	
-	private void creteProducts() {
-		this.products = new ArrayList<Item>();
-		this.products.add(new Product("Ammoniaca"));
-		this.products.add(new Product("Pane"));
-		this.products.add(new Product("Candeggina"));
-		this.products.add(new Product("Acqua"));
-		this.products.add(new Product("Zucchine"));
-		this.products.add(new Product("Pasta"));
-	}
-	
-	private void createOrders() {
-		this.orders = new ArrayList<Item>();
-		this.orders.add(new Order(this.products.subList(2, 5), this.users.get(0)));
-		this.orders.add(new Order(this.products.subList(1, 3), this.users.get(1)));
-		this.orders.add(new Order(this.products.subList(3, 5), this.users.get(2)));
-		this.orders.add(new Order(this.products.subList(0, 5), this.users.get(3)));
-	}
+//	private void createUsers() {
+//		this.users = new ArrayList<Item>();
+//		this.users.add(new User("Mario", "Rossi"));
+//		this.users.add(new User("Maria", "Rossi"));
+//		this.users.add(new User("Federico", "Rossi"));
+//		this.users.add(new User("Federica", "Rossi"));
+//	}
+//	
+//	private void creteProducts() {
+//		this.products = new ArrayList<Item>();
+//		this.products.add(new Product("Ammoniaca"));
+//		this.products.add(new Product("Pane"));
+//		this.products.add(new Product("Candeggina"));
+//		this.products.add(new Product("Acqua"));
+//		this.products.add(new Product("Zucchine"));
+//		this.products.add(new Product("Pasta"));
+//	}
+//	
+//	private void createOrders() {
+//		this.orders = new ArrayList<Item>();
+//		this.orders.add(new Order(this.products.subList(2, 5), this.users.get(0)));
+//		this.orders.add(new Order(this.products.subList(1, 3), this.users.get(1)));
+//		this.orders.add(new Order(this.products.subList(3, 5), this.users.get(2)));
+//		this.orders.add(new Order(this.products.subList(0, 5), this.users.get(3)));
+//	}
 	
 	public List<Item> getUsers() {
 		return this.users;
