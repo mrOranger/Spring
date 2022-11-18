741f45c263108e6e039fd0a1f57f829e01cd1602
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-18 10:46:40 +0100

    Create first two routes for User

--- spring-data-access/src/main/java/it/edoardo/springdataaccess/controller/rest/user/UserController.java
+++ spring-data-access/src/main/java/it/edoardo/springdataaccess/controller/rest/user/UserController.java
@@ -1,21 +1,31 @@
 package it.edoardo.springdataaccess.controller.rest.user;
 
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.edoardo.springdataaccess.dao.UserService;
 import it.edoardo.springdataaccess.model.User;
 
 @RestController
 @RequestMapping(path = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 	
-	@PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public User addUser(@RequestBody User user) {
-		return user;
+	@Autowired private UserService userService;
+	
+	@GetMapping(path = "/")
+	public List<User> getUsers() {
+		return this.userService.getUsers();
 	}
-
+	
+	@GetMapping(path = "/:id/")
+	public User getUser(@PathParam(value = "id") int userId) {
+		return this.userService.getUser(userId);
+	}
 }
