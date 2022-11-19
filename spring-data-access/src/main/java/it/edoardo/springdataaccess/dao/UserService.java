7b4b6b13c25b9b83858c5cf4e55e3d1bee809440
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-19 11:39:19 +0100

    Update queries to remove refernces

--- spring-data-access/src/main/java/it/edoardo/springdataaccess/dao/UserService.java
+++ spring-data-access/src/main/java/it/edoardo/springdataaccess/dao/UserService.java
@@ -17,10 +17,13 @@ public class UserService implements UserDAO {
 	private JdbcTemplate connection;
 
 	private static final String ADD_USER = "INSERT INTO users (id, first_name, last_name, date_of_birth, tax_code) VALUES(?, ?, ?, ?, ?)"
-			+ "	ON DUPLICATE KEY UPDATE first_name = ?, last_name = ?, date_of_birth = ?, tax_code = ?";
+			+ "	ON DUPLICATE KEY UPDATE id = ?, first_name = ?, last_name = ?, date_of_birth = ?, tax_code = ?";
 	private static final String GET_USERS = "SELECT id, first_name, last_name, date_of_birth, tax_code FROM users";
 	private static final String GET_USER = "SELECT id, first_name, last_name, date_of_birth, tax_code FROM users WHERE id = ?";
+	private static final String DELETE_USERS_REFERENCE = "DELETE FROM orders_products";
+	private static final String DELETE_ORDERS_REFERENCE = "DELETE FROM orders";
 	private static final String DELETE_USERS = "DELETE FROM users";
+	private static final String DELETE_USER_REFERENCE = "DELETE FROM orders_products OP, orders O WHERE OP.order_id = O.id AND O.customer = ?";
 	private static final String DELETE_USER = "DELETE FROM users WHERE id = ?";
 
 	public UserService(DataSource dataSource) {
@@ -45,8 +48,8 @@ public class UserService implements UserDAO {
 
 	@Override
 	public void addUser(User user) throws DataAccessException {
-		this.connection.update(ADD_USER, user.getId(), user.getFirstName(), user.getLastName(), user.getDateOfBirth().toString(),
-				user.getTaxCode());
+		this.connection.update(ADD_USER, user.getId(), user.getFirstName(), user.getLastName(), user.getDateOfBirth().toString(), user.getTaxCode(),
+				user.getId(), user.getFirstName(), user.getLastName(), user.getDateOfBirth().toString(), user.getTaxCode());
 	}
 
 	@Override
@@ -62,11 +65,14 @@ public class UserService implements UserDAO {
 
 	@Override
 	public void deleteUsers() throws DataAccessException {
+		this.connection.update(DELETE_USERS_REFERENCE);
+		this.connection.update(DELETE_ORDERS_REFERENCE);
 		this.connection.update(DELETE_USERS);
 	}
 
 	@Override
 	public void deleteUser(int id) throws DataAccessException {
+		this.connection.update(DELETE_USER_REFERENCE, id);
 		this.connection.update(DELETE_USER, id);
 	}
 }
