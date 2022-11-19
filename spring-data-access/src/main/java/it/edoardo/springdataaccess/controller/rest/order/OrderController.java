6e7c72a6ce7bc29dd851f978a371d70d6f014281
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-19 10:53:59 +0100

    Complete controller for orders

--- spring-data-access/src/main/java/it/edoardo/springdataaccess/controller/rest/order/OrderController.java
+++ spring-data-access/src/main/java/it/edoardo/springdataaccess/controller/rest/order/OrderController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +30,35 @@ public class OrderController {
 	@GetMapping(path = "/{id}/")
 	public Order getOrder(@PathVariable(name = "id") int id) {
 		return this.connection.getOrder(id);
+	}
+	
+	@PostMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Order postOrder(@RequestBody Order order) {
+		this.connection.addOrder(order);
+		return order;
+	}
+	
+	@PutMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public List<Order> putOrders(@RequestBody List<Order> orders) {
+		this.connection.updateOrders(orders);
+		return orders;
+	}
+	
+	@PutMapping(path = "/{id}/", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Order putOrder(@PathVariable(name = "id") int id, @RequestBody Order order) {
+		this.connection.updateOrder(id, order);
+		return order;
+	}
+	
+	@DeleteMapping(path = "/")
+	public List<Order> deleteOrders() {
+		this.connection.deleteOrders();
+		return null;
+	}
+	
+	@DeleteMapping(path = "/{id}/")
+	public Order deleteOrder(@PathVariable(name = "id") int id) {
+		this.connection.deleteOrder(id);
+		return null;
 	}
 }
