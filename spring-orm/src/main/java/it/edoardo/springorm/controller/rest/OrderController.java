571c30bef78e9a06cff1689ada5747e9b8de0b99
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-21 10:41:09 +0100

    Injection of repository variables

--- spring-orm/src/main/java/it/edoardo/springorm/controller/rest/OrderController.java
+++ spring-orm/src/main/java/it/edoardo/springorm/controller/rest/OrderController.java
@@ -1,11 +1,16 @@
 package it.edoardo.springorm.controller.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.edoardo.springorm.repository.OrderRepository;
+
 @RestController
 @RequestMapping(path = "/orders", produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderController {
+	
+	@Autowired private OrderRepository repository;
 
 }
--- spring-orm/src/main/java/it/edoardo/springorm/controller/rest/ProductController.java
+++ spring-orm/src/main/java/it/edoardo/springorm/controller/rest/ProductController.java
@@ -1,11 +1,16 @@
 package it.edoardo.springorm.controller.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.edoardo.springorm.repository.ProductRepository;
+
 @RestController
 @RequestMapping(path = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProductController {
+	
+	@Autowired private ProductRepository repository;
 
 }
