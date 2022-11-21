3a12438a263c751c57ee2958343d1b690e9df4cc
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-21 10:18:35 +0100

    Add basic REST controllers

---
+++ spring-orm/src/main/java/it/edoardo/springorm/controller/rest/OrderController.java
@@ -0,0 +1,11 @@
+package it.edoardo.springorm.controller.rest;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/orders", produces = MediaType.APPLICATION_JSON_VALUE)
+public class OrderController {
+
+}
