f8eaa65fa948405049e0ad18076a148877caa903
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-23 09:17:09 +0100

    Removed unused imports and close project

--- spring-orm/src/main/java/it/edoardo/springorm/controller/rest/OrderController.java
+++ spring-orm/src/main/java/it/edoardo/springorm/controller/rest/OrderController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import org.omg.CORBA.CurrentOperations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
