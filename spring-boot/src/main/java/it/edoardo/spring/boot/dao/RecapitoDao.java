2a64a8b3c4d1e02ee9adb5967a48d3f5c1c435ca
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-24 11:50:34 +0100

    Created methods for DAO Recapito

--- spring-boot/src/main/java/it/edoardo/spring/boot/dao/RecapitoDao.java
+++ spring-boot/src/main/java/it/edoardo/spring/boot/dao/RecapitoDao.java
@@ -1,5 +1,19 @@
 package it.edoardo.spring.boot.dao;
 
+import java.util.List;
+
+import it.edoardo.spring.boot.model.Recapito;
+
 public interface RecapitoDao {
+	
+	public abstract List<Recapito> getRecapiti();
+	public abstract Recapito getRecapito(int id);
+	public abstract Recapito getRecapitoOfImpiegato(int id);
+	public abstract Recapito getRecapitoByEmail(String email);
+	public abstract Recapito getRecapitoByTelefono(String telefono);
+	public abstract List<Recapito> getRecapitiByProvider(String provider);
+	public abstract List<Recapito> getRecapitiByDipartimento(int id);
+	public abstract List<Recapito> getRecapitiOfDirigenti();
+	public abstract List<Recapito> getRecapitiOfDirigente(int id);
 
 }
