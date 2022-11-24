efc04d52e0fbca663314bf91e1aee53366d3137b
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-24 12:56:32 +0100

    Defined routes for GET methods in ImpiegatiController

--- spring-boot/src/main/java/it/edoardo/spring/boot/rest/ImpiegatiController.java
+++ spring-boot/src/main/java/it/edoardo/spring/boot/rest/ImpiegatiController.java
@@ -4,69 +4,88 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.edoardo.spring.boot.dao.impl.ImpiegatoService;
 import it.edoardo.spring.boot.model.Impiegato;
 
-@RestController 
+@RestController @RequestMapping(path = "/impiegati/") 
 public class ImpiegatiController {
 	
 	@Autowired private ImpiegatoService service;
 	
+	@GetMapping(path = "/")
 	public List<Impiegato> getImpiegati(){
 		return this.service.getImpiegati();
 	}
 	
-	public Impiegato getImpiegato(int id) {
+	@GetMapping(path = "/{id}/")
+	public Impiegato getImpiegato(@PathVariable(name = "id") int id) {
 		return this.service.getImpiegatoById(id);
 	}
 	
-	public Impiegato getImpiegatoByCf(String cf) {
+	@GetMapping(path = "/cf/{cf}/")
+	public Impiegato getImpiegatoByCf(@PathVariable(name = "cf") String cf) {
 		return this.service.getImpiegatoByCf(cf);
 	}
 	
-	public List<Impiegato> getImpiegatiByData(LocalDate start, LocalDate end) {
+	@GetMapping(path = "/data/{start}/{end}/")
+	public List<Impiegato> getImpiegatiByData(@PathVariable(name = "start") LocalDate start, 
+			@PathVariable(name = "end") LocalDate end) {
 		return this.service.getImpiegatiByData(start, end);
 	}
 	
-	public List<Impiegato> getImpiegatiByVia(String via, int civico) {
+	@GetMapping(path = "/via/{via}/{civico}/")
+	public List<Impiegato> getImpiegatiByVia(@PathVariable(name = "via") String via, 
+			@PathVariable(name = "civico") int civico) {
 		return this.service.getImpiegatiByVia(via, civico);
 	}
 	
-	public List<Impiegato> getImpiegatiByCittà(String città) {
+	@GetMapping(path = "/città/{città}/")
+	public List<Impiegato> getImpiegatiByCittà(@PathVariable(name = "città") String città) {
 		return this.service.getImpiegatiByCittà(città);
 	}
 	
-	public List<Impiegato> getImpiegatiByProvincia(String provincia) {
+	@GetMapping(path = "/provincia/{provincia}/")
+	public List<Impiegato> getImpiegatiByProvincia(@PathVariable(name = "provincia") String provincia) {
 		return this.service.getImpiegatiByProvincia(provincia);
 	}
 	
-	public List<Impiegato> getImpiegatiByRegione(String regione) {
+	@GetMapping(path = "/regione/{regione}/")
+	public List<Impiegato> getImpiegatiByRegione(@PathVariable(name = "regione") String regione) {
 		return this.service.getImpiegatiByRegione(regione);
 	}
 	
-	public List<Impiegato> getImpiegatiByNazione(String nazione) {
+	@GetMapping(path = "/nazione/{nazione}/")
+	public List<Impiegato> getImpiegatiByNazione(@PathVariable(name = "nazione") String nazione) {
 		return this.service.getImpiegatiByNazionalità(nazione);
 	}
 	
-	public Impiegato getImpiegatoByEmail(String email) {
+	@GetMapping(path = "/email/{email}/")
+	public Impiegato getImpiegatoByEmail(@PathVariable(name = "email") String email) {
 		return this.service.getImpiegatoByEmail(email);
 	}
 	
-	public Impiegato getImpiegatoByTelefono(String telefono) {
+	@GetMapping(path = "/telefono/{telefono}/")
+	public Impiegato getImpiegatoByTelefono(@PathVariable(name = "telefono") String telefono) {
 		return this.service.getImpiegatoByTelefono(telefono);
 	}
 	
-	public List<Impiegato> getImpiegatiByDipartimento(int dipartimento) {
+	@GetMapping(path = "/dipartimento/{dipartimento}/")
+	public List<Impiegato> getImpiegatiByDipartimento(@PathVariable(name = "dipartimento") int dipartimento) {
 		return this.service.getImpiegatiByDipartimento(dipartimento);
 	}
 	
+	@GetMapping(path = "/dirigenti/")
 	public List<Impiegato> getDirigenti() {
 		return this.service.getDirigenti();
 	}
 	
-	public Impiegato getDirigente(int dipartimento) {
+	@GetMapping(path = "/dirigente/{dipartimento}/")
+	public Impiegato getDirigente(@PathVariable(name = "dipartimento") int dipartimento) {
 		return this.service.getDirigente(dipartimento);
 	}
 
