cce9d0045c1fd5efaa50d955de37640e9006bbf5
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-25 10:12:50 +0100

    Modificate query per repository Impiegato

--- spring-boot/src/main/java/it/edoardo/spring/boot/repository/ImpiegatoRepository.java
+++ spring-boot/src/main/java/it/edoardo/spring/boot/repository/ImpiegatoRepository.java
@@ -14,43 +14,43 @@ public interface ImpiegatoRepository extends JpaRepository<Impiegato, Integer>{
 	public abstract Optional<Impiegato> findByCodiceFiscale(String codiceFiscale);
 	public abstract List<Impiegato> findAllByDataDiNascitaBetween(LocalDate start, LocalDate end);
 	
-	@Query(value = "SELECT I.* FROM impiegati I, indirizzi Ind WHERE I.id_indirizzo = Ind.id_indirizzo AND Ind.numero = ?2 AND Ind.via = ?1",
+	@Query(value = "SELECT I.* FROM impiegati I, indirizzi Ind WHERE I.id_indirizzo = Ind.id AND Ind.numero = ?2 AND Ind.via = ?1",
 			nativeQuery = true)
 	public abstract List<Impiegato> findAllByViaAndNumeroCivico(String via, int civico);
 	
-	@Query(value = "SELECT I.* FROM impiegati I, indirizzi Ind WHERE I.id_indirizzo = Ind.id_indirizzo AND Ind.città = ?1",
+	@Query(value = "SELECT I.* FROM impiegati I, indirizzi Ind WHERE I.id_indirizzo = Ind.id AND Ind.città = ?1",
 			nativeQuery = true)
 	public abstract List<Impiegato> findAllByCittà(String città);
 	
-	@Query(value = "SELECT I.* FROM impiegati I, indirizzi Ind WHERE I.id_indirizzo = Ind.id_indirizzo AND Ind.regione = ?1",
+	@Query(value = "SELECT I.* FROM impiegati I, indirizzi Ind WHERE I.id_indirizzo = Ind.id AND Ind.regione = ?1",
 			nativeQuery = true)
 	public abstract List<Impiegato> findAllByRegione(String regione);
 	
-	@Query(value = "SELECT I.* FROM impiegati I, indirizzi Ind WHERE I.id_indirizzo = Ind.id_indirizzo AND Ind.provincia = ?1",
+	@Query(value = "SELECT I.* FROM impiegati I, indirizzi Ind WHERE I.id_indirizzo = Ind.id AND Ind.provincia = ?1",
 			nativeQuery = true)
 	public abstract List<Impiegato> findAllByProvincia(String provincia);
 	
-	@Query(value = "SELECT I.* FROM impiegati I, indirizzi Ind WHERE I.id_indirizzo = Ind.id_indirizzo AND Ind.nazione = ?1",
+	@Query(value = "SELECT I.* FROM impiegati I, indirizzi Ind WHERE I.id_indirizzo = Ind.id AND Ind.nazione = ?1",
 			nativeQuery = true)
 	public abstract List<Impiegato> findAllByNazione(String nazione);
 	
-	@Query(value = "SELECT I.* FROM impiegati I, recapiti R WHERE I.id_impiegato = R.id_impiegato AND R.email = ?1",
+	@Query(value = "SELECT I.* FROM impiegati I, recapiti R WHERE I.id = R.id_impiegato AND R.email = ?1",
 			nativeQuery = true)
 	public abstract Optional<Impiegato> findByEmail(String email);
 	
-	@Query(value = "SELECT I.* FROM impiegati I, recapiti R WHERE I.id_impiegato = R.id_impiegato AND R.telefono = ?1",
+	@Query(value = "SELECT I.* FROM impiegati I, recapiti R WHERE I.id = R.id_impiegato AND R.telefono = ?1",
 			nativeQuery = true)
 	public abstract Optional<Impiegato> findByTelefono(String telefono);
 	
-	@Query(value = "SELECT I.* FROM impiegati I, dipartimenti D WHERE I.id_dipartimento = D.id_dipartimento AND D.id_dipartimento = ?1",
+	@Query(value = "SELECT I.* FROM impiegati I, dipartimenti D WHERE I.id_dipartimento = D.id AND D.id = ?1",
 			nativeQuery = true)
 	public abstract List<Impiegato> findAllByDipartimento(int dipartimento);
 	
-	@Query(value = "SELECT I.* FROM impiegati I, dipartimenti D WHERE I.id_dipartimento = D.id_dipartimento AND D.direttore = I.id_impiegato",
+	@Query(value = "SELECT I.* FROM impiegati I, dipartimenti D WHERE I.id_dipartimento = D.id AND D.direttore = I.id",
 			nativeQuery = true)
 	public abstract List<Impiegato> findAllDirigenti();
 	
-	@Query(value = "SELECT I.* FROM impiegati I, dipartimenti D WHERE I.id_dipartimento = D.id_dipartimento AND D.id_dipartimento = ?1 AND D.direttore = I.id_impiegato",
+	@Query(value = "SELECT I.* FROM impiegati I, dipartimenti D WHERE I.id_dipartimento = D.id AND D.id = ?1 AND D.direttore = I.id",
 			nativeQuery = true)
 	public abstract Optional<Impiegato> findDirigenteInDipartimento(int dipartimento);
 
