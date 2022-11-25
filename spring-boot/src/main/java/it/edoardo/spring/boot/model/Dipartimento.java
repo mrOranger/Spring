90cb32dc199502a0f021d8a06e83d46081c8c588
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-25 15:20:32 +0100

    Removed @OneToMany relationship with Impiegato

--- spring-boot/src/main/java/it/edoardo/spring/boot/model/Dipartimento.java
+++ spring-boot/src/main/java/it/edoardo/spring/boot/model/Dipartimento.java
@@ -1,8 +1,5 @@
 package it.edoardo.spring.boot.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,7 +7,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -21,17 +17,10 @@ public class Dipartimento {
 	@Column(name = "id") private int id;
 	@Column(name = "nome") private String nome;
 	
-	@OneToMany(mappedBy = "lavoraIn", cascade = CascadeType.ALL, orphanRemoval = false)
-	private List<Impiegato> impiegati;
-	
 	@OneToOne(cascade = CascadeType.ALL) 
 	@JoinColumn(name = "direttore")
 	private Impiegato direttore;
-	
-	public Dipartimento() {
-		this.impiegati = new ArrayList<>();
-	}
-	
+
 	public int getId() {
 		return id;
 	}
@@ -52,33 +41,7 @@ public class Dipartimento {
 		return direttore;
 	}
 	
-	public List<Impiegato> getImpiegati() {
-		return impiegati;
-	}
-
-	public void setImpiegati(List<Impiegato> impiegati) {
-		this.impiegati = impiegati;
-	}
-
 	public void setDirettore(Impiegato direttore) {
 		this.direttore = direttore;
-	}
-	
-	public void addDirettore(Impiegato direttore) {
-		addDirettore(direttore, true);
-	}
-	
-	public void addDirettore(Impiegato direttore, boolean inserito) {
-		if(direttore != null) {
-            if(getImpiegati().contains(direttore)) {
-            	getImpiegati().set(getImpiegati().indexOf(direttore), direttore);
-            }
-            else {
-            	getImpiegati().add(direttore);
-            }
-            if (inserito) {
-            	direttore.setDirettore(this, false);
-            }			
-		}
 	}
 }
