2db66bbe4e8b82605d52bcd082f305f5d9ba9d36
mrOrange <35071573+mrOranger@users.noreply.github.com>
2022-11-26 13:27:18 +0100

    Modified queries for RecapitoRepository

--- spring-boot/src/main/java/it/edoardo/spring/boot/repository/RecapitoRepository.java
+++ spring-boot/src/main/java/it/edoardo/spring/boot/repository/RecapitoRepository.java
@@ -14,20 +14,20 @@ public interface RecapitoRepository extends JpaRepository<Recapito, Integer>{
 	public abstract Optional<Recapito> findByTelefono(String telefono);
 	public abstract List<Recapito> findAllByEmailContaining(String provider);
 	
-	@Query(value = "SELECT R.* FROM recapiti R JOIN impiegati I "
-			+ "WHERE I.id_impiegato = ?1", nativeQuery = true)	
+	@Query(value = "SELECT R.* FROM recapiti R, impiegati I "
+			+ "WHERE I.id = ?1 AND R.id_impiegato = I.id", nativeQuery = true)	
 	public abstract List<Recapito> findAllByImpiegato(int id);
 	
-	@Query(value = "SELECT R.* FROM recapiti R JOIN impiegati I JOIN dipartimenti D "
-			+ "WHERE D.id_dipartimento = ?1", nativeQuery = true)
+	@Query(value = "SELECT R.* FROM recapiti R, impiegati I "
+			+ "WHERE I.lavora_in = ?1 AND R.id_impiegato = I.id", nativeQuery = true)
 	public abstract List<Recapito> findAllByDipartimento(int id);
 
-	@Query(value = "SELECT R.* FROM recapiti R JOIN impiegati I JOIN dipartimenti D "
-			+ "WHERE D.dirigente = I.id_impiegato", nativeQuery = true)
+	@Query(value = "SELECT R.* FROM recapiti R, impiegati I "
+			+ "WHERE R.id_impiegato = I.id AND I.dirige IS NOT NULL", nativeQuery = true)
 	public abstract List<Recapito> findAllOfDirigenti();
 	
-	@Query(value = "SELECT R.* FROM recapiti R JOIN impiegati I JOIN dipartimenti D "
-			+ "WHERE D.dirigente = I.id_impiegato AND D.id_dipartimento = ?1", nativeQuery = true)
+	@Query(value = "SELECT R.* FROM recapiti R, impiegati I "
+			+ "WHERE R.id_impiegato = I.id AND I.dirige = ?1", nativeQuery = true)
 	public abstract List<Recapito> findAllOfDirigente(int id);
 	
 }
